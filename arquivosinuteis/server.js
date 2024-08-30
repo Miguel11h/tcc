94eacbe5bc63bc9c45bedbfdd015d18fc6e19853
strelinha <stelarosak@gmail.com>
2024-08-30 10:50:14 -0300

    Update server.js

--- arquivosinuteis/server.js
+++ arquivosinuteis/server.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'formulario.html'))
+    res.sendFile(path.join(__dirname, 'public', 'cadastro.html'))
 });
 
 app.post('/send-information', (req, res) => {
