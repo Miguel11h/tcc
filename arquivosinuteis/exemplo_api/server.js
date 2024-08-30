1e19199d807b0f464be9e96abc4c7c5a42f71b8b
miguel11h <miguel.santos.silva.07@gmail.com>
2024-08-30 12:03:17 -0300

    adição do db

--- arquivosinuteis/exemplo_api/server.js
+++ arquivosinuteis/exemplo_api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const path = require('path');
 
@@ -26,14 +27,35 @@ app.post('/send-information', (req, res) => {
             message:'As senhas digitadas não são iguais'
         })
     }
+    
 
     else{
-        // aqui vai o codigo para inserir os registros no banco de dados
+        // aqui vai o código para inserir os registros no banco de dados
+        const db = new sqlite3.Database('./db/banco.db', (err) => {
+            if(err){
+                return console.error(err.message);
+            }
+                console.log("Conectou com o banco de dados!");
+        });
+
+        db.run('INSERT INTO usuario(nome, email, senha) VALUES (?, ?, ?)',
+        [nome, email, senha], (err) => {
+            if(err){
+                console.error(err.message);
+            }
+        });
+        
+        db.close((err) => {
+            if(err){
+                return console.error(err.message);
+            }
+                console.log("Fechou a conexão com o banco de dados!");
+        });
         res.status(200).json({
-            status: 'sucess',
-            message:'Registro feito com sucesso!',
+            status: 'success',
+            message: 'Registro feito com sucesso!',
             campos: req.body
-        })
+        });
     }
     
     res.send(req.body);
